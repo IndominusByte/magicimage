b37c497ac226f4fe3f903d0c681fc10ec218a583
IndominusByte <nyomanpradipta120@gmail.com>
2021-11-06 21:08:34 +0800

    add utils test

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/IndominusByte/magicimage
+
+go 1.17
+
+require (
+	github.com/corona10/goimagehash v1.0.3 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,106 @@
 package magicimage
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRenameFolderAndFile(t *testing.T) {
+	// path not exists
+	t.Run("path not exists", func(t *testing.T) {
+		result := RenameFolderAndFile("test", "test")
+		assert.EqualError(t, result, "stat test: no such file or directory")
+	})
+	// path with sub not exists
+	t.Run("path with sub not exists", func(t *testing.T) {
+		result := RenameFolderAndFile("test/testing", "test/testing")
+		assert.EqualError(t, result, "stat test/testing: no such file or directory")
+	})
+
+	os.MkdirAll("test/testing", os.ModePerm)
+	// success rename folder
+	t.Run("success rename folder", func(t *testing.T) {
+		result := RenameFolderAndFile("test", "test-change")
+		assert.Nil(t, result)
+		assert.DirExists(t, "test-change")
+	})
+	// success rename folder with sub
+	t.Run("success rename folder with sub", func(t *testing.T) {
+		result := RenameFolderAndFile("test-change/testing", "test-change/testing-change")
+		assert.Nil(t, result)
+		assert.DirExists(t, "test-change/testing-change")
+	})
+
+	f, e := os.Create("test-change/filename.txt")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, "Hello")
+
+	f, e = os.Create("test-change/testing-change/filename.txt")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, "Hello")
+
+	// success rename file
+	t.Run("success rename file", func(t *testing.T) {
+		result := RenameFolderAndFile("test-change/filename.txt", "test-change/filename-change.txt")
+		assert.Nil(t, result)
+		assert.FileExists(t, "test-change/filename-change.txt")
+	})
+	// success rename file with sub
+	t.Run("success rename file with sub", func(t *testing.T) {
+		result := RenameFolderAndFile("test-change/testing-change/filename.txt", "test-change/testing-change/filename-change.txt")
+		assert.Nil(t, result)
+		assert.FileExists(t, "test-change/testing-change/filename-change.txt")
+	})
+
+	os.RemoveAll("test-change")
+}
+
+func TestDeleteFolderAndFile(t *testing.T) {
+	// path not exists
+	t.Run("path not exists", func(t *testing.T) {
+		result := DeleteFolderAndFile("test")
+		assert.EqualError(t, result, "stat test: no such file or directory")
+	})
+	// path with sub not exists
+	t.Run("path with sub not exists", func(t *testing.T) {
+		result := DeleteFolderAndFile("test/testing")
+		assert.EqualError(t, result, "stat test/testing: no such file or directory")
+	})
+
+	os.MkdirAll("test/testing", os.ModePerm)
+	f, e := os.Create("test/filename.txt")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, "Hello")
+
+	f, e = os.Create("test/testing/filename.txt")
+	if e != nil {
+		panic(e)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, "Hello")
+
+	// delete file
+	t.Run("delete file", func(t *testing.T) {
+		result := DeleteFolderAndFile("test/testing/filename.txt")
+		assert.Nil(t, result)
+		assert.NoFileExists(t, "test/testing/filename.txt")
+	})
+	// delete folder
+	t.Run("delete folder", func(t *testing.T) {
+		result := DeleteFolderAndFile("test")
+		assert.Nil(t, result)
+		assert.NoDirExists(t, "test")
+	})
+}
